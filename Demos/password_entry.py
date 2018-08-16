b83bf99f1e43e6952a8cf00a8a4d18315b9d1ce6
PatersonConnor <connor.paterson@my.jcu.edu.au>
2018-08-16 14:38:10 +1000

    add password checking program

--- Demos/password_entry.py
+++ Demos/password_entry.py
@@ -1,1 +1,8 @@
 """Connor Paterson"""
+MIN_LENGTH = 3
+userpass = len(input("please enter a password:"))
+while userpass < MIN_LENGTH:
+    print("A password must contain at least 3 characters.")
+    userpass = len(input("please enter a password:"))
+print("*" * userpass)
+
